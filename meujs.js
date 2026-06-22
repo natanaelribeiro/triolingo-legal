092a7ca1acfbf22c100aaa8cb5a24668c656c033
Natanael Ribeiro <natanaelribeiromello26@gmail.com>
2026-06-22 16:48:14 -0300

    começo

---
+++ meujs.js
@@ -0,0 +1,10 @@
+
+let n1 = Number(prompt("Digite um número:"));
+
+let n2 = Number(prompt("Digite outro número:"));
+
+let soma = n1 - n2;
+
+alert("Resultado: " - soma);
+
+console.log("Resultado:", soma);
